fixed mixed precision

src/modules/model.py
@@ -400,17 +400,17 @@ class ViT(nn.Module):
             module.bias.data.zero_()
             module.weight.data.fill_(1.0)
         elif isinstance(module, Embeddings):
-            module.position_embeddings.data = nn.init.trunc_normal_(
-                module.position_embeddings.data.to(torch.float32),
+            nn.init.trunc_normal_(
+                module.position_embeddings,
                 mean=0.0,
                 std=self.config["initializer_range"],
-            ).to(module.position_embeddings.dtype)
+            )
 
-            module.cls_token.data = nn.init.trunc_normal_(
-                module.cls_token.data.to(torch.float32),
+            nn.init.trunc_normal_(
+                module.cls_token,
                 mean=0.0,
                 std=self.config["initializer_range"],
-            ).to(module.cls_token.dtype)
+            )
 
     def _set_module_names(self):
         """

src/modules/train.py
@@ -69,7 +69,7 @@ class Trainer:
 
             self.optimizer.zero_grad()
 
-            with torch.cuda.amp.autocast():
+            with torch.amp.autocast(device_type=self.device.type):
                 outputs, _ = self.model(images)
                 loss = self.criterion(outputs, labels)
 
@@ -126,7 +126,7 @@ class Trainer:
                 images = images.to(self.device, non_blocking=True)
                 labels = labels.to(self.device, non_blocking=True)
 
-                with torch.cuda.amp.autocast():
+                with torch.amp.autocast(device_type=self.device.type):
                     outputs, _ = self.model(images)
                     loss = self.val_criterion(outputs, labels)
 